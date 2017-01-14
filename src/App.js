db64d6c2cbf94fbc850c8eb31429b7171f5c0921
Jeffrey Canty <hello@jeffrey.engineer>
2017-01-14 16:23:40 -0600

    Create `<Card />` component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Card from './Card';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>App</h1>
+        <Card />
       </div>
     );
   }
