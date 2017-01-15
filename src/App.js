111fce37b7e295206cd92524bfe9ae8e53d56010
Jeffrey Canty <hello@jeffrey.engineer>
2017-01-14 21:43:25 -0600

    :rocket: Add `css-modules`. Update components.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
-import './App.css';
+import CSSModules from 'react-css-modules';
+import styles from './App.css';
 import Card from './Card';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div styleName="App">
         <Card />
       </div>
     );
   }
 }
 
-export default App;
+export default CSSModules(App, styles);
